f16d96d098a73ebe502886586493db72c4aa35e1
Hyunhoi Koo <hoikoo00@gmail.com>
2023-05-21 17:15:41 +0000

    Refactor: Create upload_cpython_runtime() convenience function, Add function docstrings for faasm library functions.

--- invoke_simple.py
+++ invoke_simple.py
@@ -3,29 +3,21 @@ import logging
 from pathlib import Path
 from time import sleep
 
-from lib.constants import CPYTHON_WASM_FILE
-from lib.faasm import encode_input, invoke_py_func, upload_func, upload_py_func
+from lib.faasm import invoke_py_func, upload_cpython_runtime, upload_py_func
 
 logger = logging.getLogger(__name__)
 
-
-CPYTHON_USER = "python"
-CPYTHON_FUNC = "py_func"
 INVOKE_DELAY = 2
 
 
-def test_upload_invoke_simple_func(fn_file: Path, input_data_str: str) -> None:
-    # Upload the CPython runtime.
-    upload_func(CPYTHON_WASM_FILE, user=CPYTHON_USER, fn_name=CPYTHON_FUNC)
+def test_upload_invoke_simple_func(fn_file: Path, input_data: str) -> None:
+    upload_cpython_runtime()
 
     # Read the Python function file at `fn_file` and upload the parsed Python function.
     fn_name = upload_py_func(fn_file.read_text(encoding="utf-8"))
 
     logger.info(f"Waiting for {INVOKE_DELAY} seconds for the upload to be propagated...")
     sleep(INVOKE_DELAY)
-
-    # Encode the function invocation arguments.
-    input_data = encode_input(input_data_str)
 
     # Invoke uploaded function.
     output = invoke_py_func(fn_name, input_data=input_data)
@@ -48,10 +40,10 @@ def main() -> None:
         error_msg = f"Provided file {func_file!r} does not exist!"
         logger.error(error_msg)
         raise FileNotFoundError(error_msg)
-    input_data_str: str = args.input
+    input_data: str = args.input
 
     # Run the test.
-    test_upload_invoke_simple_func(func_file, input_data_str)
+    test_upload_invoke_simple_func(func_file, input_data)
 
 
 if __name__ == "__main__":
--- lib/constants.py
+++ lib/constants.py
@@ -2,3 +2,5 @@ import os
 from pathlib import Path
 
 CPYTHON_WASM_FILE = Path(os.getenv("CPYTHON_WASM_FILE", default="./data/cpython.wasm"))
+CPYTHON_USER = "python"
+CPYTHON_FUNC = "py_func"
