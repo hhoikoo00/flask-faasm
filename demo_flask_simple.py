84d3e6285e8074379a21030d438699286c90b37f
Hyunhoi Koo <hoikoo00@gmail.com>
2023-05-23 15:43:38 +0000

    Add command-line arguments for host and port of webapp

--- demo_flask_simple.py
+++ demo_flask_simple.py
@@ -11,13 +11,13 @@ logger = logging.getLogger(__name__)
 APP_OBJECT_MAP = {"simple_app": simple_app}
 
 
-def demo_flask_faasm(app: Flask) -> None:
+def demo_flask_faasm(app: Flask, host: str, port: int) -> None:
     process_flask_app(app)
 
     # Launch the modified webapp.
     app.run(
-        host="0.0.0.0",
-        port=8000,
+        host=host,
+        port=port,
         debug=True,
     )
 
@@ -28,6 +28,10 @@ def main() -> None:
     # Parse command-line arguments.
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--app", type=str, default="simple_app", help="Name of the webapp to demo with.")
+    arg_parser.add_argument("--host", type=str, default="127.0.0.1", help="Host to run the webapp on.")
+    arg_parser.add_argument(
+        "--port", type=int, default=8000, help="Port to run the webapp on. Avoid 8002 and 8080 as Faasm uses it."
+    )
     args = arg_parser.parse_args()
 
     app_str: str = args.app
@@ -36,9 +40,11 @@ def main() -> None:
         logger.error(error_msg)
         raise FileNotFoundError(error_msg)
     app = APP_OBJECT_MAP[app_str]
+    host: str = args.host
+    port: int = args.port
 
     # Run the demo with given `app`.
-    demo_flask_faasm(app)
+    demo_flask_faasm(app, host, port)
 
 
 if __name__ == "__main__":
