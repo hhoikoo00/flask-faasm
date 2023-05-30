11f0bf30e02ade061c7374cbdc38f6957a1f5deb
Hyunhoi Koo <hoikoo00@gmail.com>
2023-05-30 14:35:21 +0000

    Temporarily remove http request endpoint as it's not working on faasm

--- webapp/simple/app.py
+++ webapp/simple/app.py
@@ -1,6 +1,3 @@
-from http.client import HTTPResponse
-from urllib.request import Request, urlopen
-
 from flask import Flask, request
 
 app = Flask(__name__)
@@ -35,28 +32,3 @@ def request_body_stuff():
     else:
         request_body = request.get_data()
     return f"Body of the request given was {request_body}"
-
-
-@app.post("/http/echo")
-def make_http_request_to_echo():
-    """
-    Demo showing how HTTP request should be made in a Faasm environment.
-    User must ensure that the echo webapp must be hosted (at `./webapp/echo/app.py`).
-    """
-
-    ECHO_HOST = "http://127.0.0.1"
-    ECHO_PORT = 1500
-    ECHO_ENDPOINT = ""
-
-    # Encode the data into bytes. `as_text` flag purposefully used to emphasise that
-    # the data needs to be encoded into bytes for the constructor of Request() object.
-    data_str = request.get_data(as_text=True)
-    data = data_str.encode(encoding="utf-8")
-
-    echo_request = Request(f"{ECHO_HOST}:{ECHO_PORT}/{ECHO_ENDPOINT}", data=data)
-    echo_response: HTTPResponse
-    with urlopen(echo_request) as echo_response:
-        data = echo_response.read()
-        data_str = data.decode(encoding="utf-8")
-
-    return data
